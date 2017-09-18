7503b7becb902ceac2cca522e757055b714e22ee
Catt Small <cattsmall@gmail.com>
2017-09-17 22:38:28 -0400

    remove old content in preparation for new version
    All irrelevant files have been deleted. If we need
    any of the old files, they are accessible via the
    gh-pages branch. This branch will be empty (minus
    documentation files and favicons) so the new files can be added.

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'https://rubygems.org'
-
-group :jekyll_plugins do
-  gem 'github-pages'
-  gem 'jekyll-paginate'
-  gem 'jekyll-assets'
-  gem 'jekyll-contentblocks'
-end
--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-var DonationCloseButtonCompany = document.getElementsByClassName("company-results_donation-close");
-
-DonationCloseButtonCompany[0].addEventListener("click", function( event ) {
-  var DonationBannerCompany = document.getElementsByClassName("company-results_donation");
-  DonationBannerCompany[0].className += " company-results_donation-hidden";
-  console.log("hide");
-}, false);
