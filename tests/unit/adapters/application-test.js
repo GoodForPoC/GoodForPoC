689588189b416cd310a40753f78547d18d3a1eaf
Jacky Alciné <yo@jacky.wtf>
2017-10-14 14:29:59 -0700

    test(adapter): Update checks.

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -1,8 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('adapter:application', 'Unit | Adapter | application', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
+  needs: [],
 });
 
 // Replace this with your real tests.
