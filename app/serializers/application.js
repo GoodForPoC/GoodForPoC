90d8f43ee3c8c4c801182dc97e5918c43043a8f5
Jacky Alciné <yo@jacky.wtf>
2017-12-19 23:57:43 -0800

    fix(serializer): Remove diff text

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,8 +1,3 @@
 import { Serializer } from 'ember-graphql-adapter';
 
-<<<<<<< HEAD
-export default Serializer.extend({
-});
-=======
 export default Serializer.extend({});
->>>>>>> feat(gql): Add adapter + serializer.
